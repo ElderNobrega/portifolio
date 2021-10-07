e9e24f07286888e6beaab307e8d2f48697af3e69
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-10-06 20:47:01 -0400

    Adding comment to the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,9 @@ import './header.styles.scss'
 const Header = () => {
   return (
     <div className='header'>
-      {/* Just using a mark up for for the header.. It will be replaced with links later one! */}
+      {/* Just using a mark up for for the header.. It will be replaced with links later one! 
+          Still think on how I will organize and structure my portifolio.
+      */}
       <div className='logo-container'>
         <div className='logo'>Logo</div>
         <div className='logo'>Resume</div>
