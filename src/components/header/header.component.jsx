a768fc55748a88dcad0ab529b5b8ce8f3f7ce414
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-10-06 20:42:11 -0400

    Adding keyframes to the welcome to my portifolio

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,15 @@ import './header.styles.scss'
 const Header = () => {
   return (
     <div className='header'>
-      This is a header
+      <div className='logo-container'>
+        <div className='logo'>Logo</div>
+        <div className='logo'>Resume</div>
+      </div>
+      <div className='options'>
+        <div className='option'>About me</div>
+        <div className='option'>Contact</div>
+        <div className='option'>Projects</div>
+      </div>
     </div>
   )
 }
--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -2,8 +2,17 @@ import './welcome.styles.scss'
 
 const Welcome = () => {
   return (
-    <div className='welcome'>
-      Welcome to my portifolio
+    <div className='welcome-box'>
+      {/* <div className='welcome'>
+        Welcome to <span className='name'>Elder Guerra</span> portifolio
+      </div> */}
+      <div className='name-container'>
+        <span className='name'>Elder Guerra</span>
+      </div>
+      <div className='welcome-container'>
+        <span className='welcome'>Welcome to</span>
+        <span className='portifolio'>Portifolio</span>
+      </div>
     </div>
   )
 }
